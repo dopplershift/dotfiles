4fa20ca19e04de9f6973031495fd2f48ee5e7af8
Ryan May <rmay31@gmail.com>
2022-02-15 15:58:38 -0700

    More fixes for install script

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-SRCDIR=`dirname "$0"`
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install our customizations and plugin submodules for oh-my-zsh
+# Clean out what oh-my-zsh left
+cd ~
 rm ~/.zshrc
 rm -rf ~/.oh-my-zsh/custom
+
+# Get the location of the install script (and hence git repo)
+SRCDIR=`dirname "$0"`
+
+# Set up zsh customizations
 ln -sf $SRCDIR/custom ~/.oh-my-zsh/
 
 # Install vim plugins
@@ -20,3 +25,6 @@ ln -sf $SRCDIR/.gitconfig ~
 ln -sf $SRCDIR/.vimrc ~
 ln -sf $SRCDIR/.zprofile ~
 ln -sf $SRCDIR/.zshrc ~
+
+# Reload zsh
+exec /bin/zsh
